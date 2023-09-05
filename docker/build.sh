40ed7239640a8a3e306c648bd30e50fa9a61263f
Michael Spector <spektom@gmail.com>
2023-09-05 13:49:48 +0300

    Fixed CI image

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,16 @@ tar -zcf tarball.tgz events-generator
 rm -rf events-generator
 trap "rm -f tarball.tgz" EXIT
 
-docker build \
+if which docker >/dev/null 2>&1; then
+  DOCKER=docker
+elif which podman >/dev/null 2>&1; then
+  DOCKER=podman
+else
+  echo "ERROR: either docker or podman must be installed!"
+  exit 1
+fi
+
+$DOCKER build \
   -t viyadb/events-generator:latest \
   -t viyadb/events-generator:$(cat ../VERSION) \
   .
